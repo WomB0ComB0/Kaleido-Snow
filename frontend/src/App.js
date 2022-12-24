d3d3c78463437fca3fb0fad18e42c2fbbd323bb8
PhysBoom <mc_gold05@yahoo.com>
2022-12-23 17:25:15 -0800

    Added deso login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ButtonPrimary } from "./components/Buttons";
+import Deso from "deso-protocol";
 
 function App() {
+  const deso = new Deso();
+
+  async function loginWithDeso(){
+    const user = await deso.identity.login();
+    console.log(user);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ButtonPrimary onClick={loginWithDeso}>Login with Deso</ButtonPrimary>
   );
 }
 
