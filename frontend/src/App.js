4744ca8c1c722d09549ea5978d8b1670bcb51312
PhysBoom <mc_gold05@yahoo.com>
2022-12-23 22:41:26 -0800

    Added snowfall

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { ButtonPrimary } from "./components/Buttons";
+import Snowfall from "react-snowfall";
 import Deso from "deso-protocol";
+import "./App.css"
 
 function App() {
   const deso = new Deso();
@@ -11,7 +13,17 @@ function App() {
   }
 
   return (
-    <ButtonPrimary onClick={loginWithDeso}>Login with Deso</ButtonPrimary>
+    <div className="dark-container">
+      <Snowfall style={{
+          position: 'fixed',
+          width: '100vw',
+          height: '100vh',
+        }}
+      />
+      {/*<ButtonPrimary onClick={loginWithDeso}>Login with Deso</ButtonPrimary>*/}
+      {/* Image on the bottom (snow ground) */}
+      <img src={require("./images/snow-ground.png")} alt="Snow ground" className="snow-ground" style={{position: 'fixed', bottom: 0, left: 0, width: '100vw', height: "40vh"}}/>
+    </div>
   );
 }
 
