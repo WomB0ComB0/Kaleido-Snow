5f5655b27242701d731e8fe34356d4ad48266d3e
Mike Odnis <airwolf635@gmail.com>
2022-12-25 23:06:09 -0500

    yeah umm

--- frontend/public/Draw.js
+++ frontend/public/Draw.js
@@ -102,22 +102,19 @@ function startAnimation() {
   startAnimation();
   
   window.addEventListener("resize", startAnimation);
-// Preloader fade-out
-// const preloader = document.querySelector('.preloader');
+// Preloader
+const preloader = document.querySelector('.preloader');
 
-// const fadeEffect = setInterval(() => {
-//   // if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
-//   if (!preloader.style.opacity) {
-//     preloader.style.opacity = 1;
-//   }
-//   if (preloader.style.opacity > 0) {
-//     preloader.style.opacity -= 0.1;
-//   } else {
-//     clearInterval(fadeEffect);
-//   }
-// }, 200);
+const fadeEffect = setInterval(() => {
+  // if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+  }
+  if (preloader.style.opacity > 0) {
+    preloader.style.opacity -= 0.1;
+  } else {
+    clearInterval(fadeEffect);
+  }
+}, 200);
 
-// window.addEventListener('load', fadeEffect);
-
-
-// Audio 
+window.addEventListener('load', fadeEffect);
